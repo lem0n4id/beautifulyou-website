be45c6efc66709110b19f9c4264c1818f6e8433a
Lenin Kennedy <leninkennedy4work@gmail.com>
2024-01-21 13:19:27 +0530

    add fonts using google fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			fontFamily: {
-				"josefin": ["Josefin Sans Variable", ...defaultTheme.fontFamily.sans],
-				"playfair": ["Playfair Display Variable", ...defaultTheme.fontFamily.sans],
+				"josefin": ["Josefin Sans", "sans-serif"],
+				"playfair": ["Playfair Display", "sans-serif"],
 			},
 		},
 	},
