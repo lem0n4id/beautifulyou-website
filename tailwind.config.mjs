242814bf8a0705a11c568c8c08c1443ac98df281
Lenin Kennedy <leninkennedy4work@gmail.com>
2024-01-21 13:45:32 +0530

    feat: add fonts (#1)
    * feat: install fonts
    
    * feat: import fonts
    
    * feat: check the fonts
    
    * fix: font config
    
    * add fonts using google fonts
    
    * fix: font?
    
    * fix: font?
    
    * fix: font?
    
    * fix: font?
    
    * fix: uncomment css
    
    * fix
    
    * fix: remove important tag
    
    * fix: uninstall fontsource packages

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,12 +3,15 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				josefin: ["Josefin Sans", "sans-serif"],
+				playfair: ["Playfair Display", "sans-serif"],
+			},
 			colors:{
 				primary:"#FDE7E6",
 				secondary:"#FCC6C5",
 				gradient:"#FDE7E6",
 			},
-		
 		},
 	},
 	plugins: [],
