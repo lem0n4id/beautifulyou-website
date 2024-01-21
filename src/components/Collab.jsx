2cb824a0474447ee917cfee124d38ea7389704e0
Lenin Kennedy <leninkennedy4work@gmail.com>
2024-01-22 04:04:07 +0530

    fix(ui): collab component - desktop

--- src/components/Collab.jsx
+++ src/components/Collab.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Collab() {
   return (
-    <div className='collab-gradient h-content w-full flex items-center justify-around gap-10 md:gap-2 px-44 py-20 md:flex-nowrap flex-wrap-reverse'>
-        <div></div>
-        <div className='h-content w-full flex flex-col justify-center items-center md:items-start gap-4'>
+    <section>
+      <div className='collab-gradient h-content w-full flex items-center justify-around gap-10 md:gap-2 px-44 py-20 md:flex-nowrap flex-wrap-reverse'>
+        <div className='h-content w-fit flex flex-col justify-center items-center md:items-start gap-4'>
             <div className='text-base text-black font-thin font-josefin'>COME COLLABORATE WITH US</div>
             <div className='text-6xl font-medium font-playfair leading-10'>COLLABORATE</div>
             <div className='text-6xl font-medium font-playfair leading-10'>W/ US</div>
@@ -13,7 +13,9 @@ function Collab() {
             <div><button className='bg-black w-36 h-11  text-white text-lg hover:bg-black/80 00'>Contact Us</button></div>
         </div>
         <div class="w-96 h-96 bg-stone-50"></div>
-    </div>
+      </div>
+    </section>
+    
   )
 }
 
