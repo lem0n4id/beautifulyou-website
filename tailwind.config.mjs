7ee5c49839cd4e2e3134db3c7177e5dd2be6ea82
Lenin Kennedy <leninkennedy4work@gmail.com>
2024-01-21 13:01:59 +0530

    feat: import fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				"josefin": ["Josefin Sans", ...defaultTheme.fontFamily.sans],
+				"playfair": ["Playfair Display", ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [],
 }
