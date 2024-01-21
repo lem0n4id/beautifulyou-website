832d6385135d901e58d896efe1b0ef4a6f116edc
Lenin Kennedy <leninkennedy4work@gmail.com>
2024-01-21 14:34:04 +0530

    fix: site link to test preview image

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://beautifulyou-website.vercel.app/',
+  site: 'https://beautifulyou-website-git-add-meta-tags-lem0n4id.vercel.app/',
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });
