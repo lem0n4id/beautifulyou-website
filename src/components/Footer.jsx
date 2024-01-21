a8e3a928226f4291c76776f2156e2ad16635a396
Kadambini30 <kadambinitrivedi@gmail.com>
2024-01-22 02:14:23 +0530

    feat: footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+
+function Footer() {
+	return (
+		<div className="bg-primary h-60 w-full border-2 border-black text-josefin flex justify-evenly items-center">
+			<div className="flex flex-col  gap-3">
+				<div className="text-xl">Beautiful You</div>
+				<div>
+					<a className="text-black" href="beautifulyou@gmail.com">
+						beautifulyou@gmail.com
+					</a>
+				</div>
+				<div className="w-96 flex justify-between text-lg font-normal font-josefin ">
+					<div className="cursor-pointer">Podcast</div>
+					<div className="cursor-pointer">Blog</div>
+					<div className="cursor-pointer">Events</div>
+					<div className="cursor-pointer">Volunteer</div>
+					<div className="cursor-pointer">About Us</div>
+				</div>
+			</div>
+			<div className="flex items-end h-[7rem] gap-2">
+				<a
+					className="text-black "
+					href="https://www.youtube.com/@beautifulyou2321"
+					target="_blank"
+				>
+					<svg
+						className="h-8 w-8"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+					>
+						<path
+							fill="currentColor"
+							d="m10 15l5.19-3L10 9zm11.56-7.83c.13.47.22 1.1.28 1.9c.07.8.1 1.49.1 2.09L22 12c0 2.19-.16 3.8-.44 4.83c-.25.9-.83 1.48-1.73 1.73c-.47.13-1.33.22-2.65.28c-1.3.07-2.49.1-3.59.1L12 19c-4.19 0-6.8-.16-7.83-.44c-.9-.25-1.48-.83-1.73-1.73c-.13-.47-.22-1.1-.28-1.9c-.07-.8-.1-1.49-.1-2.09L2 12c0-2.19.16-3.8.44-4.83c.25-.9.83-1.48 1.73-1.73c.47-.13 1.33-.22 2.65-.28c1.3-.07 2.49-.1 3.59-.1L12 5c4.19 0 6.8.16 7.83.44c.9.25 1.48.83 1.73 1.73"
+						/>
+					</svg>
+				</a>
+				<a
+					className="text-black "
+					href="https://open.spotify.com/show/6BoCbJ8lvEvFTprfpo25Oc"
+					target="_blank"
+				>
+					<svg
+						className="h-8 w-8"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+					>
+						<path
+							fill="currentColor"
+							d="M17.9 10.9C14.7 9 9.35 8.8 6.3 9.75c-.5.15-1-.15-1.15-.6c-.15-.5.15-1 .6-1.15c3.55-1.05 9.4-.85 13.1 1.35c.45.25.6.85.35 1.3c-.25.35-.85.5-1.3.25m-.1 2.8c-.25.35-.7.5-1.05.25c-2.7-1.65-6.8-2.15-9.95-1.15c-.4.1-.85-.1-.95-.5c-.1-.4.1-.85.5-.95c3.65-1.1 8.15-.55 11.25 1.35c.3.15.45.65.2 1m-1.2 2.75c-.2.3-.55.4-.85.2c-2.35-1.45-5.3-1.75-8.8-.95c-.35.1-.65-.15-.75-.45c-.1-.35.15-.65.45-.75c3.8-.85 7.1-.5 9.7 1.1c.35.15.4.55.25.85M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2"
+						/>
+					</svg>
+				</a>
+				<a
+                    className="text-black "
+					href="https://www.linkedin.com/company/beautiful-you23/"
+					target="_blank"
+				>
+					<svg
+                    className="h-8 w-8"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+					>
+						<path
+							fill="currentColor"
+							d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z"
+						/>
+					</svg>
+				</a>
+				<a
+					className="text-black "
+					href="https://www.instagram.com/beautifulyou.pod/"
+					target="_blank"
+				>
+					<svg
+						className="h-8 w-8"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+					>
+						<path
+							fill="currentColor"
+							d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"
+						/>
+					</svg>
+				</a>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
